1484fdb21e8f981696b511360e6e4039a32a540e
dun <DundeeDevelops@gmail.com>
2023-07-19 14:54:24 -0500

    Added markdown editor and preview components

---
+++ src/components/Editor/Editor.js
@@ -0,0 +1,11 @@
+
+ 
+function Editor() {
+  return (
+     <textarea id='editor' rows={60} cols={80}>
+      ### This will be the editor!
+     </textarea>
+  );
+}
+
+export default Editor;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+
+import Editor from './components/Editor/Editor';
+import Preview from './components/Preview/Preview';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
-    <App /> 
+    <Editor /> 
+    <Preview />
   </React.StrictMode>
 );
 
