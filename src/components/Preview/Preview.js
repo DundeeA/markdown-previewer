1de6604ef015efa29d1606aa8f675f518ab33f1b
dun <DundeeDevelops@gmail.com>
2023-07-19 14:54:21 -0500

    Added markdown editor and preview components

---
+++ src/components/Preview/Preview.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Preview = (props) => {
+    return(
+    <div id="preview">
+
+     This is the previewer!
+
+    </div>
+    )
+}
+
+
+export default Preview;
