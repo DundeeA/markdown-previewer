92744ea9c7d1d729b1fa806994738007ea8b191f
dun <DundeeDevelops@gmail.com>
2023-07-20 22:07:06 -0500

    Added lots of styles for editor and preview

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,11 +1,10 @@
 import React from "react";
+import './Editor.css'
 
 function Editor(props) {
   return (
     <textarea
       id="editor"
-      rows={60}
-      cols={80}
       onChange={props.onChange}
       defaultValue={props.defaultText}
     >
